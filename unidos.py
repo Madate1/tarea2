d7c77232719f056fa68f06e0bf35d32c2cdb9fe1
Mateo Morocho <mateodvd@gmail.com>
2022-11-11 13:52:55 -0500

    cambio

--- unidos.py
+++ unidos.py
@@ -7,7 +7,7 @@ class mascotas:
         self.sonido = sonido 
     
      def presetarse(self):
-        return f"!Hola¡{self.sonido} es toy feliz de verte me llamo {self.nombre}" 
+        return f"!Hola¡ {self.sonido} estoy feliz de verte, me llamo {self.nombre}" 
 
 class gato(mascotas):
     pass
@@ -16,23 +16,16 @@ class gato(mascotas):
         return f"Soy de raza {raza}"
 
     def saludo(self):
-        return f"Soy {self.nombre} mi sonido es {self.sonido} pertenesco a la clase " 
+        return f"Soy {self.nombre} mi sonido es {self.sonido}" 
 
-class perro(mascotas):
+class perro(gato):
     pass
-
-    def descripcion(self, raza):
-        return f"Soy de raza {raza}"
-
-    def saludo(self):
-        return f"Soy {self.nombre} mi sonido es {self.sonido} pertenesco a la clase "   
-
 
 gato1 = gato("Nieves","miau")
 print(gato1.presetarse())
 print(gato1.saludo())
 print(gato1.descripcion("Burmés"))
-print("<------------------------------------->")
+print("<=====================================================>")
 perro1 = perro("Steven","woooof")
 print(perro1.presetarse())
 print(perro1.saludo())
